2b01b1d17ec5ba9c4ae9f6c263c68bec3788bd01
Adam Paley <apaley95@gmail.com>
2024-11-24 18:31:53 -0500

    init commit

--- models/prediction.js
+++ models/prediction.js
@@ -1,24 +1,24 @@
-// // import
-// const mongoose = require("mongoose")
+// import
+const mongoose = require("mongoose")
 
-// // contenderSchema
-// const contenderSchema = new mongoose.Schema({
-//     name: String,
-//     rank: { type: Number, min: 1, max: 5},
-//     points: Number
-// })
+// contenderSchema
+const contenderSchema = new mongoose.Schema({
+    name: String,
+    rank: { type: Number, min: 1, max: 5},
+    points: Number
+})
 
-// // schmea
-// const predictionSchema = new mongoose.Schema({
-//     category: String,
-//     user: String, //{ type: mongoose.Schema.Types.ObjectId, ref: "User" },
-//     contenders: [contenderSchema]
-// })
+// schmea
+const predictionSchema = new mongoose.Schema({
+    category: String,
+    user: String, //{ type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    contenders: [contenderSchema]
+})
 
-// // prevent multiple predictions per user per category
-// predictionSchema.index({ user: 1, category: 1 }, { unique: true })
+// prevent multiple predictions per user per category
+predictionSchema.index({ user: 1, category: 1 }, { unique: true })
 
-// const Prediction = mongoose.model("Prediction", predictionSchema)
+const Prediction = mongoose.model("Prediction", predictionSchema)
 
-// // exports
-// module.exports = Prediction
+// exports
+module.exports = Prediction
